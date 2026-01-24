f65371dd15f0246f87b9991621423cd91a09ff19
Sanatani-Dhruv <dhruvdds125@gmail.com>
2026-01-24 22:50:10 +0530

    Now came to know need to change whole routing architecture to make routing dynamic

--- app/Router/Route.php
+++ app/Router/Route.php
@@ -6,62 +6,73 @@ class Route {
 	public $method;
 	public $file;
 	static $uris;
+	private $viewName_methodCall;
+	private $displayedView;
 
 	function __construct() {
 		if (!isset($_SERVER["PATH_INFO"])) {
 			$_SERVER["PATH_INFO"] = "/";
 		}
+
+		$this->displayedView = 0;
 	}
 
 	public function get(string $uri, $viewName_methodCall) {
 		global $uris;
 		$uris[$uri] = $viewName_methodCall;
-		if ($_SERVER["PATH_INFO"] == $uri && strtoupper($_SERVER["REQUEST_METHOD"]) == 'GET') {
-			// If String is passed, it should be viewname, so view will be rendered
-			if (is_string($viewName_methodCall)) {
-				$file_name = __DIR__ . "/../../resources/views/" . "$viewName_methodCall";
-				// echo $file_name;
-				$file_exist_status = (file_exists($file_name)) ? 1 : 0;
-				if ($file_exist_status) {
-					require($file_name);
-				} else {
-					if (file_exists(__DIR__ . "/../Helper/Views/view-notfound-error.php")) {
-						require(__DIR__ . "/../Helper/Views/view-notfound-error.php");
-					} else {
-						echo "<h1>View with name <code>`$viewName_methodCall`</code> not Found.<h1>";
-					}
-				}
-				// If Array is passed, then it should be for calling method of a Controller class, and also we pass variables in form of key value pair array
-			} elseif (is_array($viewName_methodCall)) {
-				// echo "<pre>";
-				// print_r($viewName_methodCall);
-				// echo "</pre>";
-
-				if (is_string($viewName_methodCall[0]) && is_string($viewName_methodCall[1]) && is_array($viewName_methodCall[2])) {
-					// echo "First is string<br>";
-					// echo "Second is string<br>";
-					// echo "Third is Array<br>";
-					$arrayClass = $viewName_methodCall[0];
-					$arrayMethod = $viewName_methodCall[1];
-					$arrayObject = new ($arrayClass);
-					// print_r($arrayObject);
-					// echo $arrayMethod;
-					try {
-						extract($viewName_methodCall[2]);
-						$arrayObject->{$arrayMethod}();
-					} catch (Exception $err) {
-						echo $err;
-					}
-					// print_r($arrayObject->$arrayMethod);
-					$isAssociative = array_values($viewName_methodCall[2]) !== $viewName_methodCall[2];
-
-				}
-
-			}
-		}
+		$this->viewName_methodCall = $viewName_methodCall;
 	} 
 
 	public function __destruct() {
+	if ($this->displayedView == 0) {
+		foreach ($GLOBALS["uris"] as $uri => $array) {
+			if ($_SERVER["PATH_INFO"] == $uri && strtoupper($_SERVER["REQUEST_METHOD"]) == 'GET') {
+				// If String is passed, it should be viewname, so view will be rendered
+				if (is_string($this->viewName_methodCall)) {
+					$file_name = __DIR__ . "/../../resources/views/" . "$viewName_methodCall";
+					// echo $file_name;
+					$file_exist_status = (file_exists($file_name)) ? 1 : 0;
+					if ($file_exist_status) {
+						$this->displayedView = 1;
+						require($file_name);
+					} else {
+						if (file_exists(__DIR__ . "/../Helper/Views/view-notfound-error.php")) {
+							require(__DIR__ . "/../Helper/Views/view-notfound-error.php");
+						} else {
+							echo "<h1>View with name <code>`$viewName_methodCall`</code> not Found.<h1>";
+						}
+					}
+					// If Array is passed, then it should be for calling method of a Controller class, and also we pass variables in form of key value pair array
+				} elseif (is_array($this->viewName_methodCall)) {
+					// echo "<pre>";
+					// print_r($viewName_methodCall);
+					// echo "</pre>";
+
+					if (is_string($this->viewName_methodCall[0]) && is_string($this->viewName_methodCall[1]) && is_array($this->viewName_methodCall[2])) {
+						// echo "First is string<br>";
+						// echo "Second is string<br>";
+						// echo "Third is Array<br>";
+						$arrayClass = $this->viewName_methodCall[0];
+						$arrayMethod = $this->viewName_methodCall[1];
+						$arrayObject = new ($arrayClass);
+						// print_r($arrayObject);
+						// echo $arrayMethod;
+						try {
+							extract($this->viewName_methodCall[2]);
+							$arrayObject->{$arrayMethod}();
+						} catch (Exception $err) {
+							echo $err;
+						}
+						// print_r($arrayObject->$arrayMethod);
+						$isAssociative = array_values($this->viewName_methodCall[2]) !== $this->viewName_methodCall[2];
+
+					}
+				}
+			}
+		}
+	} else {
+
+	}
 		$route_set = false;
 		for ($i = 0; $i < count($GLOBALS["uris"]); $i++) {
 			if (isset($GLOBALS["uris"][$_SERVER["PATH_INFO"]])) {

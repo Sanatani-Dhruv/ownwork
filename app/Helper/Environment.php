407fc00cf936a007262599146941576879b26dce
Sanatani-Dhruv <dhruvdds125@gmail.com>
2026-02-04 12:49:17 +0530

    Now Can make Configuration based on production and development env

--- app/Helper/Environment.php
+++ app/Helper/Environment.php
@@ -8,15 +8,11 @@ class Environment {
   # Path Compared to this directory
 
   public static function setenv() {
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-
     $filepath = __DIR__ . '/../../.env';
     $env = file_get_contents($filepath);
     $lines = explode("\n",$env);
 
-    foreach($lines as $line){
+    foreach($lines as $line) {
       // echo preg_match("/([^#]+)\=(.*)/",$line,$matches);
       if(preg_match("/([^#]+)\=(.*)/", $line, $matches)) {
         // echo $line;
@@ -25,8 +21,15 @@ class Environment {
         putenv(trim($line));
       }
     } 
-  }
 
+    $dev_env = (getenv("DEV_ENV") == 'true') ? true : false;
+
+    if ($dev_env) {
+      ini_set('display_errors', '1');
+      ini_set('display_startup_errors', '1');
+      error_reporting(E_ALL);
+    }
+  }
 }
 
 ?>

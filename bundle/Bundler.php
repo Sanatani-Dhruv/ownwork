590d4c75e23719a6dd9072ff121b72efaac0849d
Dhruv <24amtics067@gmail.com>
2026-02-14 18:40:56 +0530

    Small Change

--- bundle/Bundler.php
+++ bundle/Bundler.php
@@ -6,7 +6,11 @@ use App\Helper\Environment;
 class Bundler {
 	function __construct() {
 		// require(__DIR__ . "/Routes.php");
-		require __DIR__ . '/../vendor/autoload.php';
+		try {
+			require __DIR__ . '/../vendor/autoload.php';
+		} catch (Exception $err) {
+			echo "It looks like you didn't ran Startup Command; run <code>composer run setup</code> in ownwork directory!!";
+		}
 		require __DIR__ . '/HelperFunction.php';
 		Environment::setenv();
 	}

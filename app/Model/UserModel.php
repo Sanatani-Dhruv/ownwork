41a839790b38e2ee312769e85b6bfd035b24d663
Dhruv <24amtics067@gmail.com>
2026-02-14 14:08:20 +0530

    Default Model Added

---
+++ app/Model/UserModel.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Model;
+
+use App\Router\Route;
+use App\Viewer\View;
+
+class UserModel {
+	function __construct() {
+		// Constructor
+	}
+
+	public function index() {
+		// Base Method
+	}
+}

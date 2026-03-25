b613536b33590cb5e19691aaac05253ef2fa630d
Sanatani-Dhruv <dhruvdds125@gmail.com>
2026-03-25 23:45:57 +0530

    Now View Call InstantView Method of View Class not else thing

--- bundle/HelperFunction.php
+++ bundle/HelperFunction.php
@@ -11,8 +11,8 @@ function env($data, bool $get = true, $specialChars = true) {
   return ($get) ? getenv($data) : putenv($data);
 }
 
-function view($string) {
-  require __DIR__ . "/../resources/views/$string";
+function view($string, array $args) {
+  \App\Helper\Viewer\View::instantView($string, $args);
 }
 
 function url(string $action) {
